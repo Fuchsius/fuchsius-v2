5b0bf2ceb063e91570e1a35159cd4915f7c2e9e8
Fuchsius (PVT) Ltd <dev.kalanadidulanga@gmail.com>
2025-03-18 15:52:37 +0530

    sadasd

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -3,8 +3,12 @@ const Header = () => {
     <header>
       <div className=" my-container">
         <nav>
-          <div className=" rounded-full py-">Home</div>
+          <div className=" rounded-full py-2.5 px-5 border-[#842DFF] border">
+            Home
+          </div>
         </nav>
+
+        <button>Contact Us</button>
       </div>
     </header>
   );
