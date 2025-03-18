1b747bdfd1c150befa40f03d6865ef86d7d35557
Fuchsius (PVT) Ltd <dev.kalanadidulanga@gmail.com>
2025-03-18 17:07:00 +0530

    asdasd

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,14 +1,31 @@
+import { Link } from "react-router";
+
 const Header = () => {
+  const NAV_ITEMS = [
+    { name: "Home", url: "/" },
+    { name: "About Us", url: "/about" },
+    { name: "Contact Us", url: "/contact" },
+    { name: "Services", url: "/services" },
+  ];
+
   return (
-    <header>
-      <div className=" my-container">
-        <nav>
-          <div className=" rounded-full py-2.5 px-5 border-[#842DFF] border">
-            Home
-          </div>
+    <header className=" py-10 fixed top-0 left-0 w-full z-50">
+      <div className=" my-container flex items-center justify-between">
+        <nav className=" flex gap-2.5">
+          {NAV_ITEMS.map((item, index) => (
+            <Link
+              to={item.url}
+              key={index}
+              className=" rounded-full py-2.5 px-5 border-my-purple border hover:bg-my-purple transition-all duration-300 text-nowrap"
+            >
+              {item.name}
+            </Link>
+          ))}
         </nav>
 
-        <button>Contact Us</button>
+        <button className=" rounded-full py-2.5 px-5 border-my-purple border bg-my-purple hover:bg-transparent transition-all duration-300 cursor-pointer text-nowrap">
+          Contact Us
+        </button>
       </div>
     </header>
   );
--- client/src/pages/home/home-page.tsx
+++ client/src/pages/home/home-page.tsx
@@ -4,13 +4,39 @@ const HomePage = () => {
   return (
     <>
       <Header />
+
       <div className="my-container">
-        <div className=" min-h-dvh border flex items-center justify-center">
-          <h1 className=" uppercase text-[198px] font-redHatDisplay font-extrabold text-center bg-gradient-to-r from-[#0C0618] via-[#D09BFF] to-[#0C0618] bg-clip-text text-transparent">
+        <div className=" min-h-dvh flex items-center justify-center relative">
+          <h1 className=" uppercase text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-[198px] font-redHatDisplay font-extrabold text-center bg-gradient-to-r from-my-black via-my-lightpurple to-my-black bg-clip-text text-transparent">
             FUCHSIUS
           </h1>
 
-          <img src="/logo.svg" alt="Fuchsius" className="absolute" />
+          <img
+            src="/logo.svg"
+            alt="Fuchsius"
+            className="absolute w-36 sm:w-48 md:w-60 lg:w-72 xl:w-auto"
+          />
+
+          <button className=" rounded-full bg-my-purple/25 p-[5px] absolute top-60 left-0">
+            <div className=" border border-my-purple px-2.5 py-[5px] rounded-full">
+              Next-Level Website
+            </div>
+          </button>
+
+          <div className="text-3xl md:text-4xl xl:text-5xl absolute bottom-20 left-0 font-redHatDisplay font-semibold">
+            Boost Efficiency,
+            <br /> Maximize{" "}
+            <span className=" text-my-lightpurple">Profit.</span>
+          </div>
+
+          <div className=" max-w-[300px] absolute bottom-48 right-0 flex flex-col items-start gap-8">
+            <p className=" text-lg lg:text-xl">
+              Use our AI tools to automate tasks, cut costs, and enhance your
+              bottom line.
+            </p>
+
+            <button className=" px-5 py-4 font-semibold">Discover More</button>
+          </div>
         </div>
       </div>
     </>
